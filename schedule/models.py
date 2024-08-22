e8e713920b70e30fc3aad99ddff382b67c50e614
Alexander Gedz <gedz.buh@yandex.ru>
2024-08-22 13:57:54 +0300

    Добавлены модели: пользователя, пожелания работников, график и смена

--- schedule/models.py
+++ schedule/models.py
@@ -1,2 +1,60 @@
 from django.db import models
 
+from users.models import User
+
+SHIFT_TYPES = (
+    ("morning", "Утро"),
+    ("day", "День"),
+    ("night", "Ночь"),
+    ("free", "Выходной"),
+)
+
+
+class EmployeeWish(models.Model):
+    employee = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Сотрудник"
+    )
+    date = models.DateField(verbose_name="Дата")
+    shift_preference = models.CharField(
+        max_length=20, choices=SHIFT_TYPES, verbose_name="Предпочтительный тип смены"
+    )
+
+    class Meta:
+        unique_together = ("employee", "date", "shift_preference")
+
+
+class Schedule(models.Model):
+    year = models.PositiveIntegerField(verbose_name="Год")
+    month = models.PositiveSmallIntegerField(verbose_name="Месяц")
+
+    class Meta:
+        verbose_name = "График работы"
+        verbose_name_plural = "Графики работ"
+        ordering = ["-month"]
+
+
+class Shift(models.Model):
+    schedule = models.ForeignKey(
+        Schedule, on_delete=models.CASCADE, verbose_name="График работы"
+    )
+    date = models.DateField(verbose_name="Дата смены")
+    morning_needed = models.PositiveIntegerField(
+        default=0, verbose_name="Количество сотрудников в утро"
+    )
+    day_needed = models.PositiveIntegerField(
+        default=0, verbose_name="Количество сотрудников в день"
+    )
+    night_needed = models.PositiveIntegerField(
+        default=0, verbose_name="Количество сотрудников в ночь"
+    )
+    assigned_employees = models.ManyToManyField(
+        User, verbose_name="Назначенные сотрудники", blank=True
+    )
+
+    def __str__(self):
+        return f"Смена на {self.date} (Утро: {self.morning_needed}, День: {self.day_needed}, Ночь: {self.night_needed})"
+
+    class Meta:
+        verbose_name = "Смена"
+        verbose_name_plural = "Смены"
+        ordering = ["-date"]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+from nullable import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True)
+    phone = PhoneNumberField(
+        verbose_name="Номер телефона", **NULLABLE, help_text="Введите номер телефона"
+    )
+    job_title = models.CharField(
+        max_length=50,
+        verbose_name="Должность",
+        **NULLABLE,
+        help_text="Введите вашу должность",
+    )
+    avatar = models.ImageField(
+        upload_to="users_avatars/",
+        verbose_name="Аватар",
+        **NULLABLE,
+        help_text="Загрузите изображение",
+    )
+    token = models.CharField(max_length=100, verbose_name="Token", **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return f"{self.get_full_name()}, {self.email}"
